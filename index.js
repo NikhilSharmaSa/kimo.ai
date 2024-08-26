cbf20967f85f58ba46d67337b1da0582f2199ec0
NikhilSharmaSa <112597262+NikhilSharmaSa@users.noreply.github.com>
2024-08-26 11:18:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -58,7 +58,8 @@ app.post('/courses:id/chapter:chapter_id/reviews:sno',validateReview,asyncWrap(a
 
 
 app.get('/',(req,res)=>{
-    res.send('working fine ')
+   const allData= await Course.find({})
+    res.render('course/index',{allData})
 })
 app.get('/courses', asyncWrap(async(req,res)=>{
     const allData= await Course.find({})
